a6a0f9deed422e0674f10e809e0dff8ed9757405
Julie Davison <davison.jules@gmail.com>
2019-08-16 22:32:28 -0700

    added routes and js logic

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var charFriends = [
+    {
+        name: "Mickey Mouse",
+        photo: "",
+        scores: []
+    },
+    {
+        name: "Goofy",
+        photo: "",
+        scores: []
+    },
+    {
+        name: "Peter Pan",
+        photo: "https://",
+        scores: []
+    },
+    {
+        name: "Pluto",
+        photo: "https://",
+        scores: []
+    },
+    {
+        name: "",
+        photo: "https://",
+        scores: []
+    },
+    {
+        name: "",
+        photo: "https://",
+        scores: []
+    }
+
+];
+
+module.exports = charFriends;
